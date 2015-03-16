4190d126cd8091cbaa44a6b49d208d129fbdc991
katowulf <katowulf@gmail.com>
2015-03-16 12:07:44 -0700

    Completed ui-grid example, upgraded ui-grid version.

--- ui-grid/app.js
+++ ui-grid/app.js
@@ -1,13 +1,13 @@
 
-var app = angular.module('app', ['firebase', 'ui.grid']);
+var app = angular.module('app', ['firebase', 'ui.grid', 'ui.grid.infiniteScroll']);
 
 app.controller('ctrl', function($scope, $firebaseArray) {
   $scope.world = 'world';
   var baseRef = new Firebase('https://fbutil.firebaseio.com/paginate');
   var scrollRef = new Firebase.util.Scroll(baseRef, 'number');
+
   //$scope.data = $firebaseArray(scrollRef);
   $scope.opts = {
-    data: $firebaseArray(scrollRef),
     columnDefs: [
       {name: '$id', displayName: 'ID'},
       {name: 'string'},
@@ -17,6 +17,20 @@ app.controller('ctrl', function($scope, $firebaseArray) {
     ]
   };
   $scope.opts.data = $firebaseArray(scrollRef);
-  $scope.opts.data.scroll = scrollRef.scroll;
   scrollRef.scroll.next(25);
+
+  $scope.opts.onRegisterApi = function(gridApi){
+    gridApi.infiniteScroll.on.needLoadMoreData($scope,function(){
+      scrollRef.scroll.next(25);
+    });
+    gridApi.infiniteScroll.on.needLoadMoreDataTop($scope,function(){
+      scrollRef.scroll.prev(25);
+    });
+
+    $scope.opts.data.$watch(function() {
+      $scope.$evalAsync(function() {
+        gridApi.infiniteScroll.dataLoaded();
+      });
+    });
+  };
 });
